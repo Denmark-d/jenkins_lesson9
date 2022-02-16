392bd69427c5f2db3e14705e8f6b5594f4ba5a38
daniiaadiiakova <adiyakova-d@mail.ru>
2022-02-16 18:42:02 +0300

    added steps

--- src/test/java/ru/Denmark/TestForm.java
+++ src/test/java/ru/Denmark/TestForm.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 public class TestForm extends TestBase{
 
@@ -21,31 +22,41 @@ public class TestForm extends TestBase{
     @Step ("открываем страницу https://demoqa.com/automation-practice-form")
     void firstTest() {
         System.out.println("First Test");
-        open("https://demoqa.com/automation-practice-form");
-        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
-        $("#firstName").setValue("Danny");
-        $("#lastName").setValue("Dan");
-        $("#userEmail").setValue("Danny@mail.ru");
-        $("[for='gender-radio-2']").click();
-        $("#userNumber").setValue("89371703123");
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption("July");
-        $(".react-datepicker__year-select").selectOption("1988");
-        $(".react-datepicker__day--029:not(.react-datepicker__day--outside-month)").click();
-        $("#subjectsInput").setValue("history").pressEnter();
-        $("[for='hobbies-checkbox-1']").click();
-        //$("#uploadPicture").uploadFromClasspath("custom/1 .png");
-        $("#currentAddress").setValue("Наметкина");
-        $("#state").scrollTo().click();
-        $("#stateCity-wrapper").$(byText("Uttar Pradesh")).click();
-        $("#city").click();
-        $("#stateCity-wrapper").$(byText("Lucknow")).click();
-        $("#submit").scrollTo().click();
-        $(".table table-dark table-striped table-bordered table-hover").isDisplayed();
-        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(text("Danny Dan"), text("Danny@mail.ru"));
-        $(".table-responsive").shouldHave(text("8937170312"), text("29 July,1988"), text("history"),
-                text("Наметкина"), text("Uttar Pradesh Lucknow"));
+        step("Открываем страницу", () -> {
+            open("https://demoqa.com/automation-practice-form");
+        });
+        step("Проверяем наименование формы", () -> {
+            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));;
+        });
+        step("Заполняем форму", () -> {
+                    $("#firstName").setValue("Danny");
+                    $("#lastName").setValue("Dan");
+                    $("#userEmail").setValue("Danny@mail.ru");
+                    $("[for='gender-radio-2']").click();
+                    $("#userNumber").setValue("89371703123");
+                    $("#dateOfBirthInput").click();
+                    $(".react-datepicker__month-select").selectOption("July");
+                    $(".react-datepicker__year-select").selectOption("1988");
+                    $(".react-datepicker__day--029:not(.react-datepicker__day--outside-month)").click();
+                    $("#subjectsInput").setValue("history").pressEnter();
+                    $("[for='hobbies-checkbox-1']").click();
+                    //$("#uploadPicture").uploadFromClasspath("custom/1 .png");
+                    $("#currentAddress").setValue("Наметкина");
+                    $("#state").scrollTo().click();
+                    $("#stateCity-wrapper").$(byText("Uttar Pradesh")).click();
+                    $("#city").click();
+                    $("#stateCity-wrapper").$(byText("Lucknow")).click();
+                    $("#submit").scrollTo().click();
+                    $(".table table-dark table-striped table-bordered table-hover").isDisplayed();
+                });
+
+        step("Проверяем заполненную форму", () -> {
+            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+            $(".table-responsive").shouldHave(text("Danny Dan"), text("Danny@mail.ru"));
+            $(".table-responsive").shouldHave(text("8937170312"), text("29 July,1988"), text("history"),
+                    text("Наметкина"), text("Uttar Pradesh Lucknow"));
+        });
+
     }
 
     @Test
